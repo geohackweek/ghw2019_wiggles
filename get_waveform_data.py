e15af559341fad6a4f7d680b95d2a5428f140d04
Alex Hutko <alex@iris.washingotn.edu>
2019-09-11 15:28:19 -0700

    first working draft, hopefully no bugs

--- get_waveform_data.py
+++ get_waveform_data.py
@@ -1,4 +1,4 @@
-#!/home/ahutko/anaconda3/bin/python
+#!/anaconda3/bin/python
 
 #----- import modules
 
@@ -6,17 +6,21 @@ import psycopg2
 import obspy
 from obspy import UTCDateTime
 from obspy.clients.fdsn import Client
+from time_series import *
 
 #----- parameters to modify 
 
 outputdir = '/srv/shared/wiggles'
 inputfile = 'arrivals.csv'
-TBeforeArrival = 5.
-TAfterArrival = 5.
-highpassfiltercorner = 1.0
+TBeforeArrival = 10.
+TAfterArrival = 10.
+LeapSecondFudge = 27  # Fudge factor.  Subtract this from all times.
+highpassfiltercorner = 0.3
 timebuffer = 15.
 common_sample_rate = 100.  # in Hz
 client = Client("IRIS")
+etype = 'EQP'
+label = 'test'
 
 #---- Function to read infile
 
@@ -43,8 +47,12 @@ def parse_input_file(filename):
             request[key].append(pick)
     return request
 
+#----- Start the main program
+
 thing = parse_input_file(inputfile)
-print(type(thing))
+#print(thing['THS'])
+#for row in thing[etype]:
+#    print(row['sta'])
 
 taperlen = (2./highpassfiltercorner)
 
@@ -53,27 +61,31 @@ f1 = open(label + ".out.database",'w')
 
 #----- Sweep through each arrival, download Z+N+E data, write as mseed file
 
+
+#{'event_type': 'PX', 'time': '1506558928.95844', 'sta': 'BOW', 'net': 'UW', 'loc': '', 'chan': 'EHZ', 'pick_type': 'P', 'quality': 'e', 'who': 'H'}
+
 n = 0
 downloaded_netstatloc = []
-for record in cursor:
-#  if ( n < 10 ):
-    n += 1
-    net = record[4]
-    stat = record[3]
-    chan = record[9]
-    loc = record[10]
-    phase = record[11]
-    qual = record[12]
-    if ( loc == "  " ):
+for row in thing[etype]:
+  if ( n < 4 ):
+    net = row['net']
+    stat = row['sta']
+    loc = row['loc']
+    if ( loc == "" ):
         loc = "--"
+    chan = row['chan']
+    phase = row['pick_type']
+    qual = row['quality']
     sncl = net + '.' + stat + '.' + loc + '.' + chan
     netstatloc = net + '.' + stat + '.' + loc 
     if ( netstatloc not in downloaded_netstatloc ):
         downloaded_netstatloc.append(netstatloc)
-        unixtime = record[2]
+        unixtime = float(row['time']) - LeapSecondFudge
         ut = UTCDateTime(unixtime)
-        T1 = ut - TAfterArrival - timebuffer
-        T2 = ut + TBeforeArrival + timebuffer
+        T1 = ut - TBeforeArrival - timebuffer
+        T2 = ut + TAfterArrival + timebuffer
+        print("TIMES " + str(T1) + "   " + str(ut) + "   " + str(T2) )
+#        print("TRYING: " + sncl + " " + str(ut) + " " + str(T1) + " " + str(row['time'])  )
         minlen = T2 - T1 - 1
         strdate = str(T1.year) + str(T1.month).zfill(2) + str(T1.day).zfill(2) + \
                   str(T1.hour).zfill(2) + str(T1.minute).zfill(2) + \
@@ -108,18 +120,18 @@ for record in cursor:
 
             #----- if it's acceleration data, convert to velocity
             if ( chan[1:2] == 'N' ):
-                stN.remove_response(output = 'VEL', pre_filt( 0.3, 0.5, 40., 45.))
-                stE.remove_response(output = 'VEL', pre_filt( 0.3, 0.5, 40., 45.))
-                stZ.remove_response(output = 'VEL', pre_filt( 0.3, 0.5, 40., 45.))
+                stN.remove_response(output = 'VEL', pre_filt = ( 0.3, 0.5, 40., 45.))
+                stE.remove_response(output = 'VEL', pre_filt = ( 0.3, 0.5, 40., 45.))
+                stZ.remove_response(output = 'VEL', pre_filt = ( 0.3, 0.5, 40., 45.))
             elif ( chan[1:2] == 'H' ):
                 iskip_response_removal = 1
             else:
                 iskip_this_station = 1/0
 
             #----- high-pass filter everything above 1 Hz
-            stN.highpass(highpassfiltercorner)
-            stE.highpass(highpassfiltercorner)
-            stZ.highpass(highpassfiltercorner)
+            stN.filter('highpass', freq=highpassfiltercorner)
+            stE.filter('highpass', freq=highpassfiltercorner)
+            stZ.filter('highpass', freq=highpassfiltercorner)
 
             #----- make sure it's the correct sampling rate
             correct_sample_rate(stN, common_sample_rate)
@@ -127,7 +139,7 @@ for record in cursor:
             correct_sample_rate(stZ, common_sample_rate)
 
             #----- trim data to common start/endtimes and Npoints
-            trim_to_common_times(stN, stE, stZ)
+            trim_to_common_times(stN, stE, stZ, timebuffer)
 
             #----- write out the data to an .mseed file
             stN.write(fnameN, format='MSEED')
@@ -135,8 +147,9 @@ for record in cursor:
             stZ.write(fnameZ, format='MSEED')
 
             f0.write( str(fnameN) + " " + str(fnameE) + " " + str(fnameZ) + '\n' )
-            f1.write( net + " " + stat + " " + phase + " " + str(ut)[:-1] + " " + str(qual) + '\n' )  # AZ TRO P 2016-06-10T00:03:53.808300
+            f1.write( net + " " + stat + " " + phase + " " + str(ut)[:-1] + " " + str(qual) + " " + etype + '\n' )  # AZ TRO P 2016-06-10T00:03:53.808300
             print("Downloaded ",fname)
+            n += 1
         except:
             print("Download failed for ",fname)
 
