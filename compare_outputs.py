2620f807986b3c692821f707c0d9f87ec4f54366
pasiunaite <pasgabyte@gmail.com>
2019-09-12 06:58:28 +0000

    compare outputs done, needs testing and updating dirs

--- compare_outputs.py
+++ compare_outputs.py
@@ -6,6 +6,7 @@ import os
 from datetime import datetime
 from datetime import timedelta
 
+# ---------------- input file structure -----------------
 # test.in: 
 #  line: 3 .mseed files corresponding to 1 event (N, E, Z components)
 
@@ -20,6 +21,7 @@ model_file = 'test.out'
 truth_file = 'test.out.database'
 outp_name = 'comparison.out'
 
+# ------------------
 # Read in PNSN data and store it as an array
 truth_arr = []
 with open(truth_file) as f:
@@ -27,42 +29,39 @@ with open(truth_file) as f:
         tmp = line.split()
         formatted_time = datetime.strptime(tmp[3], "%Y-%m-%dT%H:%M:%S.%f") # parse str to datetime object
         truth_arr.append([tmp[0], tmp[1], tmp[2], formatted_time, tmp[4], tmp[5]])
-
+        
 print(truth_arr)
 
 # time search algorithm for model data
-# ------ there must be more efficient way to do this?
 def search_model_file(event):
     t = event[3] 
     t_lower = t - timedelta(seconds=5)
     t_upper = t + timedelta(seconds=5) 
     with open(model_file) as f:
+        phase_check = False
         for line in f:
-            outp = [event[5], 'F', numpy.nan] # template output
+            outp = [event[5], 'N', numpy.nan] # template output: event type, phase (P/S/N), offset (float/NaN)
             tmp = line.split()
             formatted_time = datetime.strptime(tmp[3], "%Y-%m-%dT%H:%M:%S.%f") # parse str to datetime object
             tmp[3] = formatted_time
-            # if network name, seismic station name match and phase arrival time is within 10 s window, return True
+            
             if event[0] == tmp[0] and event[1] == tmp[1] and formatted_time > t_lower and formatted_time < t_upper:
-                # phase check (T if the same, F otherwise)
-                if event[2] == tmp[2]:
-                    outp[1] = 'T'
+                outp[1] = tmp[2]
                 # offset
                 offset = abs(formatted_time - t)
                 outp[2] = offset.total_seconds()
-                return [True, outp]
-    return [False, ['NOF', numpy.nan, numpy.nan]] # NOF code: not found, nan, nan
-
-# p and s wave search is messy 
-    
+                # phase check
+                if event[2] == tmp[2]:
+                    return [True, outp]
+                else:
+                    phase_check = True
+    return [phase_check, outp]
 
 # Check whether PNSN event was detected by the GDP model and write output to file
 outp_file = open(outp_name, 'w')
-
 for event in truth_arr:
     search_result = search_model_file(event)
     res = search_result[1]
-    print(res)
     outp_file.write(str(res[0]) + " " + str(res[1]) + " " + str(res[2]) + '\n' )
 outp_file.close()
 
