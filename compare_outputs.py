69fe0d338e430cbe3e900dfa9ff8a46c889a36f8
pasiunaite <pasgabyte@gmail.com>
2019-09-12 19:00:43 +0000

    needs reading from cs and dynamic dictionaries

--- compare_outputs.py
+++ compare_outputs.py
@@ -3,6 +3,7 @@ import string
 import datetime
 import sys
 import os
+import csv
 from datetime import datetime
 from datetime import timedelta
 
@@ -20,6 +21,7 @@ from datetime import timedelta
 model_file = 'test.out'
 truth_file = 'test.out.database'
 outp_name = 'comparison.out'
+fudge_factor = timedelta(seconds=27)
 
 # ------------------
 # Read in PNSN data and store it as an array
@@ -30,68 +32,63 @@ with open(truth_file) as f:
         formatted_time = datetime.strptime(tmp[3], "%Y-%m-%dT%H:%M:%S.%f") # parse str to datetime object
         truth_arr.append([tmp[0], tmp[1], tmp[2], formatted_time, tmp[4], tmp[5]])
         
-print(truth_arr)
-
-"""
-# time search algorithm for model data
-def search_model_file(event):
-    t = event[3] 
-    t_lower = t - timedelta(seconds=5)
-    t_upper = t + timedelta(seconds=5) 
-    with open(model_file) as f:
-        phase_check = False
-        for line in f:
-            outp = [event[5], 'N', numpy.nan] # template output: event type, phase (P/S/N), offset (float/NaN)
-            tmp = line.split()
-            formatted_time = datetime.strptime(tmp[3], "%Y-%m-%dT%H:%M:%S.%f") # parse str to datetime object
-            tmp[3] = formatted_time
-            
-            if event[0] == tmp[0] and event[1] == tmp[1] and formatted_time > t_lower and formatted_time < t_upper:
-                outp[1] = tmp[2]
-                # offset
-                offset = abs(formatted_time - t)
-                outp[2] = offset.total_seconds()
-                # phase check
-                if event[2] == tmp[2]:
-                    return [True, outp]
-                else:
-                    phase_check = True
-    return [phase_check, outp]
-
-"""
-  
-# create dictionary
-'''
-{
-CC-SIFT-P: [time1, time2, time3, time4],
-CC-RUSH-S: [time1,time2, time3...]
-}
-'''
-# names???
+        
 networks = ['CC', 'PP', 'KK']
 stations = ['SIFT', 'RUSH', 'PR05', 'OBSR']
 phases = ['P', 'S']
-dict = {}
+model_dict = {}
 
-# create dicitonary keys
+# create dictionary keys
 for netw in networks:
     for stat in stations:
         for phase in phases:
-            dict[netw + "-" + stat + "-" + phase] = []
+            model_dict[netw + "-" + stat + "-" + phase] = []
             
 # read model file and add parsed datetime obj to dict
 with open(model_file) as f:
         for line in f:
             tmp = line.split()
             formatted_time = datetime.strptime(tmp[3], "%Y-%m-%dT%H:%M:%S.%f") # parse str to datetime object
-            dict[tmp[0] + "-" + tmp[1] + "-" + tmp[2]].append(formatted_time) 
-print (dict)
+            model_dict[tmp[0] + "-" + tmp[1] + "-" + tmp[2]].append(formatted_time) 
 
-# Check whether PNSN event was detected by the GDP model and write output to file
+# search for arrivals within +- 5s window
+def key_lookup(event):
+    t = event[3] 
+    t_lower = t - timedelta(seconds=5)
+    t_upper = t + timedelta(seconds=5) 
+    primary_key = event[0] + "-" + event[1] + "-" + event[2]
+    opp_phase = 'P'
+    if event[2] == 'P':
+        opp_phase = 'S'
+    secondary_key = event[0] + "-" + event[1] + "-" + opp_phase
+    
+    if primary_key in model_dict.keys(): #same phase detected
+        times = [event[2], model_dict[primary_key]]
+    elif secondary_key in model_dict.keys(): # opposite phase
+        times = [opp_phase, model_dict[secondary_key]]
+    else: # not detected
+        times = ['N', [numpy.nan]] 
+    return times
+
+
 outp_file = open(outp_name, 'w')
 for event in truth_arr:
-    search_result = search_model_file(event)
-    res = search_result[1]
-    outp_file.write(str(res[0]) + " " + str(res[1]) + " " + str(res[2]) + '\n' )
+    phases_times = key_lookup(event)
+    res = phases_times[1]
+    phase = phases_times[0]
+    # filter events in +-5s time window
+    t = event[3] 
+    t_lower = t - timedelta(seconds=5)
+    t_upper = t + timedelta(seconds=5) 
+    arrivals_window = []
+    outp_file.write(str(event[5]) + " " + str(phase))
+    for time in res:
+        if time > t_lower and time < t_upper:
+            offset = abs(t - time)
+            offset = offset.total_seconds()
+            arrivals_window.append(time)
+            outp_file.write(" " + str(offset))
+    outp_file.write('\n')
 outp_file.close()
+
 
